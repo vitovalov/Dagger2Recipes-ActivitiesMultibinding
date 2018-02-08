3c7893c752671ffc3a304a59f4cf63755c9bfe35
Vito Valov <vitovalov@gmail.com>
2018-02-08 17:27:22 +0100

    simplify base, inherite from daggerAppCompatActivity

--- app/src/main/java/com/frogermcs/recipes/dagger_activities_multibinding/BaseActivity.java
+++ app/src/main/java/com/frogermcs/recipes/dagger_activities_multibinding/BaseActivity.java
@@ -1,20 +1,17 @@
 package com.frogermcs.recipes.dagger_activities_multibinding;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-import dagger.android.AndroidInjection;
 import dagger.android.support.DaggerAppCompatActivity;
 
 /**
  * Created by froger_mcs on 09/08/16.
  */
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends DaggerAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AndroidInjection.inject(this);
     }
 
 }
